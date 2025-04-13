5b51781ef7ae4b6a0e83e76567f93d64d0c3ffdb
xxuanxxx <yapyu0513@gmail.com>
2025-04-13 21:39:42 +0800

    Updated

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer>
+    <div class="container">
+      <p>&copy; 2025 CO.65 All rights reserved.</p>
+    </div>
+</footer>
+
+<style>
+    footer 
+    {
+        background: #002244;
+        color: white;
+        text-align: center;
+        padding: 20px 0 0 0;
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%
+    }
+</style>
--- shopping_cart.php
+++ shopping_cart.php
@@ -11,7 +11,8 @@
     <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 </head>
 
-<body className='snippet-body'>
+<body>
+    <!--Header will put here-->
     <div class="card">
         <div class="row">
             <div class="col-md-8 cart">
@@ -31,7 +32,7 @@
                         <div class="col">
                             <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a>
                         </div>
-                        <div class="col">&euro; 44.00 <span class="close">&#10005;</span></div>
+                        <div class="col">RM 44.00 <span class="close">&#10005;</span></div>
                     </div>
                 </div>
                 <div class="row">
@@ -44,7 +45,7 @@
                         <div class="col">
                             <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a>
                         </div>
-                        <div class="col">&euro; 44.00 <span class="close">&#10005;</span></div>
+                        <div class="col">RM 44.00 <span class="close">&#10005;</span></div>
                     </div>
                 </div>
                 <div class="row border-top border-bottom">
@@ -57,32 +58,36 @@
                         <div class="col">
                             <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a>
                         </div>
-                        <div class="col">&euro; 44.00 <span class="close">&#10005;</span></div>
+                        <div class="col">RM 44.00 <span class="close">&#10005;</span></div>
                     </div>
                 </div>
-                <div class="back-to-shop"><a href="#">&leftarrow;</a><span class="text-muted">Back to shop</span></div>
+                <div class="back-to-shop"><a href="#">&leftarrow;</a><span class="text-muted">Add more</span></div>
             </div>
             <div class="col-md-4 summary">
                 <div><h5><b>Summary</b></h5></div>
                 <hr>
                 <div class="row">
                     <div class="col" style="padding-left:0;">ITEMS 3</div>
-                    <div class="col text-right">&euro; 132.00</div>
+                    <div class="col text-right">RM 132.00</div>
                 </div>
                 <form>
-                    <p>SHIPPING</p>
-                    <select><option class="text-muted">Standard-Delivery- &euro;5.00</option></select>
+                    <p>DELIVERY</p>
+                    <select>
+                        <option class="text-muted">Standard-Delivery - RM 5.00</option>
+                        <option class="text-muted">Self Pickup</option>
+                    </select>
                     <p>GIVE CODE</p>
                     <input id="code" placeholder="Enter your code">
                 </form>
                 <div class="row" style="border-top: 1px solid rgba(0,0,0,.1); padding: 2vh 0;">
                     <div class="col">TOTAL PRICE</div>
-                    <div class="col text-right">&euro; 137.00</div>
+                    <div class="col text-right">RM 137.00</div>
                 </div>
                 <button class="btn">CHECKOUT</button>
             </div>
         </div>
-    </div>                        
+    </div>
+    <!--Footer-->
+    <?php include('footer.php');?>               
 </body>
-
 </html>
