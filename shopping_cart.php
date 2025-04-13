3031de8fbae4adbff5175c9bab248a01bf2e1f91
xxuanxxx <yapyu0513@gmail.com>
2025-04-13 17:53:32 +0800

    Updated

--- shopping_cart.php
+++ shopping_cart.php
@@ -10,6 +10,7 @@
     <link href='shopping_cart.css' rel='stylesheet'>
     <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 </head>
+
 <body className='snippet-body'>
     <div class="card">
         <div class="row">
@@ -81,7 +82,7 @@
                 <button class="btn">CHECKOUT</button>
             </div>
         </div>
-            
     </div>                        
 </body>
+
 </html>
